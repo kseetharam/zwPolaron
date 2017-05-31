42ac664821147b3c20cdf7665d97084ef90f1f01
Yulia Shchadilova <shchadilova@g.harvard.edu>
2017-05-31 16:21:27 -0400

    updated test

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
+import numpy as np
 import coherent_states as cs
 
 kcutoff = 10
 dk = 0.05
 
+Ntheta = 50
+dtheta = np.pi / (Ntheta - 1)
+
 gridk = cs.Grid()
 gridk.init1d('k', dk, kcutoff, dk)
+gridk.init1d('th', dtheta, np.pi, dtheta)
 gridk.print_arrays('k')
+gridk.print_arrays('th')
