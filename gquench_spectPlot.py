b0388eaaddcc78e14ce916a8c9477bc7a6ae81a2
Kushal Seetharam <kushalseetharam@gmail.com>
2017-06-27 14:02:30 -0400

    combining sF data

--- gquench_spectPlot.py
+++ gquench_spectPlot.py
@@ -1,8 +1,18 @@
-from timeit import default_timer as timer
 import numpy as np
 import os
 
 
 dirpath = os.path.dirname(os.path.realpath(__file__))
 datapath = dirpath + '/spectdata'
-print(datapath)
+
+
+for ind, filename in enumerate(os.listdir(datapath)):
+    print(filename)
+    dat = np.load(datapath + '/' + filename)
+    if(ind == 0):
+        sfDat = dat
+    else:
+        sfDat = np.concatenate((sfDat, dat), axis=0)
+
+
+np.savetxt(dirpath + '/mm/sfDat.dat', sfDat)
