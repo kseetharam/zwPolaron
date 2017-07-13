89524e808cf6bb50ed1c216552a47cb150985a4e
Kushal Seetharam <kushalseetharam@gmail.com>
2017-07-13 09:25:20 -0400

    fixed runscript

--- quenchscript.sh
+++ quenchscript.sh
@@ -8,8 +8,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kis@mit.edu 
 #SBATCH --open-mode=append
-#SBATCH -o /clusterdata/std/quench_%A_%a
-#SBATCH -e /clusterdata/std/quench_%A_%a
+#SBATCH -o clusterdata/std/quench_%A_%a
+#SBATCH -e clusterdata/std/quench_%A_%a
 
 module load Anaconda3/4.3.0-fasrc01
 python gquench_cluster.py
