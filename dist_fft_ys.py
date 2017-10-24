fixed factor of 2 in exp

dist_fft_ys.py
@@ -33,7 +33,7 @@ def Bk(kx, ky, kz):
 
 # Create grids
 (Lx, Ly, Lz) = (10, 10, 10)
-(dx, dy, dz) = (1e-01, 1e-01, 1e-01)
+(dx, dy, dz) = (5e-01, 5e-01, 5e-01)
 x = np.arange(- Lx, Lx + dx, dx)
 y = np.arange(- Ly, Ly + dy, dy)
 z = np.arange(- Lz, Lz + dz, dz)
@@ -44,15 +44,15 @@ kx = dkx * np.arange(- Lx / dx, Lx / dx + 1, 1)
 ky = dky * np.arange(- Ly / dy, Ly / dy + 1, 1)
 kz = dkz * np.arange(- Lz / dz, Lz / dz + 1, 1)
 
-# FFT prep
+# FFT prep - this is the same as previous one
 (Nx, Ny, Nz) = (len(x), len(y), len(z))
 
 kxfft = np.fft.fftfreq(Nx) * 2 * np.pi * Nx / Lx / 2
 kyfft = np.fft.fftfreq(Nx) * 2 * np.pi * Ny / Ly / 2
 kzfft = np.fft.fftfreq(Nx) * 2 * np.pi * Nz / Lz / 2
 
-print(kx)
-print(kxfft)
+# print(kx)
+# print(kxfft)
 
 # generation
 
@@ -94,7 +94,7 @@ beta_xyz = np.fft.fftshift(beta_xyz_0) * dkx * dky * dkz
 beta_x = beta_xyz[:, Ny // 2 + 1, Nz // 2 + 1]
 
 # Exponenciate, slice
-fexp = np.exp(beta_xyz - 2 * Nph) * decay_xyz
+fexp = np.exp(beta_xyz - Nph) * decay_xyz
 fexp_x = fexp[:, Ny // 2 + 1, Nz // 2 + 1]
 
 # Inverse Fourier transform