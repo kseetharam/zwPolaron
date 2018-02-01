34cd1b70698942b0fc127a6f1242721d7ae962ef
Kushal Seetharam <kushalseetharam@gmail.com>
2018-02-01 18:25:26 -0500

    fixed anims

--- dist_movies.py
+++ dist_movies.py
@@ -74,9 +74,9 @@ if __name__ == "__main__":
 
     # Set animation datapath and max end time
 
-    animdatapath = dirpath + '/data_qdynamics' + '/cart/realtime' + '/NGridPoints_{:.2E}/P_{:.3f}_aIBi_{:.2f}'.format(NGridPoints, P, aIBi) + '/animations'
-    if os.path.isdir(animdatapath) is False:
-        os.mkdir(animdatapath)
+    figdatapath = dirpath + '/data_qdynamics' + '/cart/realtime' + '/NGridPoints_{:.2E}/P_{:.3f}_aIBi_{:.2f}'.format(NGridPoints, P, aIBi) + '/figures'
+    if os.path.isdir(figdatapath) is False:
+        os.mkdir(figdatapath)
 
     tmax = 25
     tmax_ind = (np.abs(tgrid_coarse - tmax)).argmin()
@@ -109,7 +109,10 @@ if __name__ == "__main__":
     #     norm_text.set_text(r'$\int n_{|\vec{P_{I}}|}(t) d|\vec{P_{I}}| = $' + '{:.3f}'.format(nPIm_Tot_ctVec[i]))
 
     # anim1 = FuncAnimation(fig1, animate1, interval=50, frames=range(tmax_ind + 1))
-    # anim1.save(animdatapath + '/nPIm.gif', writer='imagemagick')
+    # # anim1.save(figdatapath + '/nPIm.gif', writer='imagemagick')
+
+    # plt.draw()
+    # plt.show()
 
     # # nPBm
 
@@ -137,7 +140,7 @@ if __name__ == "__main__":
     #     norm_text.set_text(r'$\int n_{|\vec{P_{B}}|}(t) d|\vec{P_{B}}| = $' + '{:.3f}'.format(nPBm_Tot_ctVec[i]))
 
     # anim2 = FuncAnimation(fig2, animate2, interval=50, frames=range(tmax_ind + 1))
-    # anim2.save(animdatapath + '/nPBm.gif', writer='imagemagick')
+    # anim2.save(figdatapath + '/nPBm.gif', writer='imagemagick')
 
     # plt.draw()
     # plt.show()
@@ -167,20 +170,29 @@ if __name__ == "__main__":
     # ax2.set_ylabel(r'$|\vec{P_{B}}|$')
     # ax2.set_xlabel(r'$t$')
 
-    # fig1.savefig(animdatapath + '/nPIm_2D.pdf')
-    # fig2.savefig(animdatapath + '/nPBm_2D.pdf')
+    # fig1.savefig(figdatapath + '/nPIm_2D.pdf')
+    # fig2.savefig(figdatapath + '/nPBm_2D.pdf')
 
     # plt.show()
 
     # # 2D SLICE ANIMATION
 
-    # nPI
+    # # nPI
 
     # PI_x_g, PI_z_g = np.meshgrid(PI_x, PI_z, indexing='ij')
     # fig1, ax1 = plt.subplots()
 
-    # vmin = np.min(nPI_xz_slice_ctVec[-1])
-    # vmax = np.max(nPI_xz_slice_ctVec[-1])
+    # # vmin = np.min(nPI_xz_slice_st)
+    # # vmax = np.max(nPI_xz_slice_st)
+
+    # vmin = 1
+    # vmax = 0
+    # for ind, vec in enumerate(nPI_xz_slice_ctVec):
+    #     if np.min(vec) < vmin:
+    #         vmin = np.min(vec)
+    #     if np.max(vec) > vmax:
+    #         vmax = np.max(vec)
+
     # quad = ax1.pcolormesh(PI_x_g, PI_z_g, nPI_xz_slice_ctVec[1][:-1, :-1], vmin=vmin, vmax=vmax)
     # time_text = ax1.text(0.85, 0.9, 't: {:.1f}'.format(tgrid_coarse[0]), transform=ax1.transAxes, color='r')
 
@@ -188,19 +200,31 @@ if __name__ == "__main__":
     # ax1.set_ylabel(r'$P_{I,z}$')
     # ax1.set_xlabel(r'$P_{I,x}$')
     # ax1.grid(True, linewidth=0.5)
+    # fig1.colorbar(quad, ax=ax1, extend='both')
 
     # def animate1(i):
     #     quad.set_array(nPI_xz_slice_ctVec[i][:-1, :-1].ravel())
     #     time_text.set_text('t: {:.1f}'.format(tgrid_coarse[i]))
 
     # anim1 = FuncAnimation(fig1, animate1, interval=50, frames=range(tmax_ind + 1), blit=False)
-    # anim1.save(animdatapath + '/nPI_xz.gif', writer='imagemagick')
+    # anim1.save(figdatapath + '/nPI_xz.gif', writer='imagemagick')
+
+    # #
 
     # PI_x_g, PI_y_g = np.meshgrid(PI_x, PI_y, indexing='ij')
     # fig2, ax2 = plt.subplots()
 
-    # vmin = np.min(nPI_xy_slice_ctVec[-1])
-    # vmax = np.max(nPI_xy_slice_ctVec[-1])
+    # # vmin = np.min(nPI_xy_slice_st)
+    # # vmax = np.max(nPI_xy_slice_st)
+
+    # vmin = 1
+    # vmax = 0
+    # for ind, vec in enumerate(nPI_xy_slice_ctVec):
+    #     if np.min(vec) < vmin:
+    #         vmin = np.min(vec)
+    #     if np.max(vec) > vmax:
+    #         vmax = np.max(vec)
+
     # quad = ax2.pcolormesh(PI_x_g, PI_y_g, nPI_xy_slice_ctVec[1][:-1, :-1], vmin=vmin, vmax=vmax)
     # time_text = ax2.text(0.85, 0.9, 't: {:.1f}'.format(tgrid_coarse[0]), transform=ax2.transAxes, color='r')
 
@@ -208,21 +232,31 @@ if __name__ == "__main__":
     # ax2.set_ylabel(r'$P_{I,y}$')
     # ax2.set_xlabel(r'$P_{I,x}$')
     # ax2.grid(True, linewidth=0.5)
+    # fig2.colorbar(quad, ax=ax2, extend='both')
 
     # def animate2(i):
     #     quad.set_array(nPI_xy_slice_ctVec[i][:-1, :-1].ravel())
     #     time_text.set_text('t: {:.1f}'.format(tgrid_coarse[i]))
 
     # anim2 = FuncAnimation(fig2, animate2, interval=50, frames=range(tmax_ind + 1), blit=False)
-    # anim2.save(animdatapath + '/nPI_xy.gif', writer='imagemagick')
+    # anim2.save(figdatapath + '/nPI_xy.gif', writer='imagemagick')
 
-    # nPB
+    # # nPB
 
     # PB_x_g, PB_z_g = np.meshgrid(PB_x, PB_z, indexing='ij')
     # fig3, ax3 = plt.subplots()
 
-    # vmin = np.min(nPB_xz_slice_ctVec[-1])
-    # vmax = np.max(nPB_xz_slice_ctVec[-1])
+    # # vmin = np.min(nPB_xz_slice_st)
+    # # vmax = np.max(nPB_xz_slice_st)
+
+    # vmin = 1
+    # vmax = 0
+    # for ind, vec in enumerate(nPB_xz_slice_ctVec):
+    #     if np.min(vec) < vmin:
+    #         vmin = np.min(vec)
+    #     if np.max(vec) > vmax:
+    #         vmax = np.max(vec)
+
     # quad = ax3.pcolormesh(PB_x_g, PB_z_g, nPB_xz_slice_ctVec[1][:-1, :-1], vmin=vmin, vmax=vmax)
     # time_text = ax3.text(0.85, 0.9, 't: {:.1f}'.format(tgrid_coarse[0]), transform=ax3.transAxes, color='r')
 
@@ -230,33 +264,46 @@ if __name__ == "__main__":
     # ax3.set_ylabel(r'$P_{B,z}$')
     # ax3.set_xlabel(r'$P_{B,x}$')
     # ax3.grid(True, linewidth=0.5)
+    # fig3.colorbar(quad, ax=ax3, extend='both')
 
     # def animate3(i):
     #     quad.set_array(nPB_xz_slice_ctVec[i][:-1, :-1].ravel())
     #     time_text.set_text('t: {:.1f}'.format(tgrid_coarse[i]))
 
     # anim3 = FuncAnimation(fig3, animate3, interval=50, frames=range(tmax_ind + 1), blit=False)
-    # anim3.save(animdatapath + '/nPB_xz.gif', writer='imagemagick')
+    # anim3.save(figdatapath + '/nPB_xz.gif', writer='imagemagick')
 
-    PB_x_g, PB_y_g = np.meshgrid(PB_x, PB_y, indexing='ij')
-    fig4, ax4 = plt.subplots()
+    # #
 
-    vmin = np.min(nPB_xy_slice_ctVec[-1])
-    vmax = np.max(nPB_xy_slice_ctVec[-1])
-    quad = ax4.pcolormesh(PB_x_g, PB_y_g, nPB_xy_slice_ctVec[1][:-1, :-1], vmin=vmin, vmax=vmax)
-    time_text = ax4.text(0.85, 0.9, 't: {:.1f}'.format(tgrid_coarse[0]), transform=ax4.transAxes, color='r')
+    # PB_x_g, PB_y_g = np.meshgrid(PB_x, PB_y, indexing='ij')
+    # fig4, ax4 = plt.subplots()
 
-    ax4.set_title('Phonon Transverse Momentum Distribution ' + r'($n_{P_{B,x},P_{B,y}}(t)$)')
-    ax4.set_ylabel(r'$P_{B,y}$')
-    ax4.set_xlabel(r'$P_{B,x}$')
-    ax4.grid(True, linewidth=0.5)
+    # # vmin = np.min(nPB_xy_slice_st)
+    # # vmax = np.max(nPB_xy_slice_st)
 
-    def animate4(i):
-        quad.set_array(nPB_xy_slice_ctVec[i][:-1, :-1].ravel())
-        time_text.set_text('t: {:.1f}'.format(tgrid_coarse[i]))
+    # vmin = 1
+    # vmax = 0
+    # for ind, vec in enumerate(nPB_xy_slice_ctVec):
+    #     if np.min(vec) < vmin:
+    #         vmin = np.min(vec)
+    #     if np.max(vec) > vmax:
+    #         vmax = np.max(vec)
 
-    anim4 = FuncAnimation(fig4, animate4, interval=50, frames=range(tmax_ind + 1), blit=False)
-    anim4.save(animdatapath + '/nPB_xy.gif', writer='imagemagick')
+    # quad = ax4.pcolormesh(PB_x_g, PB_y_g, nPB_xy_slice_ctVec[1][:-1, :-1], vmin=vmin, vmax=vmax)
+    # time_text = ax4.text(0.85, 0.9, 't: {:.1f}'.format(tgrid_coarse[0]), transform=ax4.transAxes, color='r')
+
+    # ax4.set_title('Phonon Transverse Momentum Distribution ' + r'($n_{P_{B,x},P_{B,y}}(t)$)')
+    # ax4.set_ylabel(r'$P_{B,y}$')
+    # ax4.set_xlabel(r'$P_{B,x}$')
+    # ax4.grid(True, linewidth=0.5)
+    # fig4.colorbar(quad, ax=ax4, extend='both')
+
+    # def animate4(i):
+    #     quad.set_array(nPB_xy_slice_ctVec[i][:-1, :-1].ravel())
+    #     time_text.set_text('t: {:.1f}'.format(tgrid_coarse[i]))
+
+    # anim4 = FuncAnimation(fig4, animate4, interval=50, frames=range(tmax_ind + 1), blit=False)
+    # anim4.save(figdatapath + '/nPB_xy.gif', writer='imagemagick')
 
     # plt.draw()
     # plt.show()
@@ -265,39 +312,43 @@ if __name__ == "__main__":
 
     # PI_x_g, PI_z_g = np.meshgrid(PI_x_st, PI_z_st, indexing='ij')
     # fig1, ax1 = plt.subplots()
-    # ax1.pcolormesh(PI_x_g, PI_z_g, nPI_xz_slice_st)
+    # q1 = ax1.pcolormesh(PI_x_g, PI_z_g, nPI_xz_slice_st)
     # ax1.set_title('Static Impurity Longitudinal Momentum Distribution ' + r'($n_{P_{I,x},P_{I,z},st}$)')
     # ax1.set_ylabel(r'$P_{I,z}$')
     # ax1.set_xlabel(r'$P_{I,x}$')
     # ax1.grid(True, linewidth=0.5)
+    # fig1.colorbar(q1, ax=ax1, extend='both')
 
     # PI_x_g, PI_y_g = np.meshgrid(PI_x_st, PI_y_st, indexing='ij')
     # fig2, ax2 = plt.subplots()
-    # ax2.pcolormesh(PI_x_g, PI_y_g, nPI_xy_slice_st)
+    # q2 = ax2.pcolormesh(PI_x_g, PI_y_g, nPI_xy_slice_st)
     # ax2.set_title('Static Impurity Transverse Momentum Distribution ' + r'($n_{P_{I,x},P_{I,y},st}$)')
     # ax2.set_ylabel(r'$P_{I,y}$')
     # ax2.set_xlabel(r'$P_{I,x}$')
     # ax2.grid(True, linewidth=0.5)
+    # fig2.colorbar(q2, ax=ax2, extend='both')
 
     # PB_x_g, PB_z_g = np.meshgrid(PB_x_st, PB_z_st, indexing='ij')
     # fig3, ax3 = plt.subplots()
-    # ax3.pcolormesh(PB_x_g, PB_z_g, nPB_xz_slice_st)
+    # q3 = ax3.pcolormesh(PB_x_g, PB_z_g, nPB_xz_slice_st)
     # ax3.set_title('Static Phonon Longitudinal Momentum Distribution ' + r'($n_{P_{B,x},P_{B,z},st}$)')
     # ax3.set_ylabel(r'$P_{B,z}$')
     # ax3.set_xlabel(r'$P_{B,x}$')
     # ax3.grid(True, linewidth=0.5)
+    # fig3.colorbar(q3, ax=ax3, extend='both')
 
     # PB_x_g, PB_y_g = np.meshgrid(PB_x_st, PB_y_st, indexing='ij')
     # fig4, ax4 = plt.subplots()
-    # ax4.pcolormesh(PB_x_g, PB_y_g, nPB_xy_slice_st)
+    # q4 = ax4.pcolormesh(PB_x_g, PB_y_g, nPB_xy_slice_st)
     # ax4.set_title('Static Phonon Transverse Momentum Distribution ' + r'($n_{P_{B,x},P_{B,y},st}$)')
     # ax4.set_ylabel(r'$P_{B,y}$')
     # ax4.set_xlabel(r'$P_{B,x}$')
     # ax4.grid(True, linewidth=0.5)
+    # fig4.colorbar(q4, ax=ax4, extend='both')
 
-    # fig1.savefig(animdatapath + '/nPI_xz_st.pdf')
-    # fig2.savefig(animdatapath + '/nPI_xy_st.pdf')
-    # fig3.savefig(animdatapath + '/nPB_xz_st.pdf')
-    # fig4.savefig(animdatapath + '/nPB_xy_st.pdf')
+    # fig1.savefig(figdatapath + '/nPI_xz_st.pdf')
+    # fig2.savefig(figdatapath + '/nPI_xy_st.pdf')
+    # fig3.savefig(figdatapath + '/nPB_xz_st.pdf')
+    # fig4.savefig(figdatapath + '/nPB_xy_st.pdf')
 
-    # plt.show()
+    # # plt.show()
