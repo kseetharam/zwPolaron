557a7aa67b7b99efbdd92713fe653ffad03fca38
Kushal Seetharam <kushalseetharam@gmail.com>
2018-02-27 15:22:18 -0500

    worse grid

--- datagen_qdynamics_sph.py
+++ datagen_qdynamics_sph.py
@@ -11,8 +11,11 @@ if __name__ == "__main__":
 
     # ---- INITIALIZE GRIDS ----
 
+    # (Lx, Ly, Lz) = (21, 21, 21)
+    # (dx, dy, dz) = (0.375, 0.375, 0.375)
+
     (Lx, Ly, Lz) = (21, 21, 21)
-    (dx, dy, dz) = (0.375, 0.375, 0.375)
+    (dx, dy, dz) = (0.5, 0.5, 0.5)
 
     xgrid = Grid.Grid('CARTESIAN_3D')
     xgrid.initArray('x', -Lx, Lx, dx); xgrid.initArray('y', -Ly, Ly, dy); xgrid.initArray('z', -Lz, Lz, dz)
@@ -65,11 +68,11 @@ if __name__ == "__main__":
 
     innerdatapath = datapath + '/spherical'
 
-    # if os.path.isdir(datapath) is False:
-    #     os.mkdir(datapath)
+    if os.path.isdir(datapath) is False:
+        os.mkdir(datapath)
 
-    # if os.path.isdir(innerdatapath) is False:
-    #     os.mkdir(innerdatapath)
+    if os.path.isdir(innerdatapath) is False:
+        os.mkdir(innerdatapath)
 
     # # ---- SINGLE FUNCTION RUN ----
 
